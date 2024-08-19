67060208e7faeb3e1ed188f5368758adebb7b9da
mekaisen <mekaisenfapsgun@gmail.com>
2024-08-19 15:45:41 +0500

    Added BookForm logic Added BookForm logic

--- frontend/src/components/BookForm/BookForm.tsx
+++ frontend/src/components/BookForm/BookForm.tsx
@@ -1,4 +1,46 @@
+import { useState } from 'react';
+import './BookForm.css';
+
 const BookForm = () => {
-	return <div className='book-form app-block'>bookform</div>;
+	const [bookName, setBookName] = useState<string>('');
+	const [author, setAuthor] = useState<string>('');
+
+	const handleSubmit = (e: React.FormEvent) => {
+		e.preventDefault();
+		console.log(author, bookName);
+		setBookName('');
+		setAuthor('');
+	};
+
+	return (
+		<div className='book-form app-block'>
+			<h2>Form Set</h2>
+			<form onSubmit={handleSubmit}>
+				<div>
+					<label htmlFor='book'>Title: </label>
+					<input
+						type='text'
+						id='book'
+						value={bookName}
+						onChange={e => {
+							setBookName(e.target.value);
+						}}
+					/>
+				</div>
+				<div>
+					<label htmlFor='author'>author: </label>
+					<input
+						type='text'
+						id='author'
+						value={author}
+						onChange={e => {
+							setAuthor(e.target.value);
+						}}
+					/>
+				</div>
+				<button type='submit'>submit</button>
+			</form>
+		</div>
+	);
 };
 export { BookForm };
